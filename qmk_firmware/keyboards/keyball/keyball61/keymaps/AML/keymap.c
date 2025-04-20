cb78eb3a746afa9399f62b6c8ea051fe84c87491
noske2801 <marukaha@gmail.com>
2025-04-20 10:42:26 +0900

    垂直スクロールの感度を上昇

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/AML/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/AML/keymap.c
@@ -65,8 +65,8 @@ const uint16_t click_layer = 4;   // マウス入力が可能になった際に�
 int16_t scroll_v_mouse_interval_counter;   // 垂直スクロールの入力をカウントする。　Counting Vertical Scroll Inputs
 int16_t scroll_h_mouse_interval_counter;   // 水平スクロールの入力をカウントする。  Counts horizontal scrolling inputs.
 
-int16_t scroll_v_threshold = 50;    // この閾値を超える度に垂直スクロールが実行される。 Vertical scrolling is performed each time this threshold is exceeded.
-int16_t scroll_h_threshold = 50;    // この閾値を超える度に水平スクロールが実行される。 Each time this threshold is exceeded, horizontal scrolling is performed.
+int16_t scroll_v_threshold = 25;    // 初期値 : 50 // この閾値を超える度に垂直スクロールが実行される。 Vertical scrolling is performed each time this threshold is exceeded.
+int16_t scroll_h_threshold = 50;    // 初期値 : 50 // この閾値を超える度に水平スクロールが実行される。 Each time this threshold is exceeded, horizontal scrolling is performed.
 
 int16_t after_click_lock_movement = 0;      // クリック入力後の移動量を測定する変数。 Variable that measures the amount of movement after a click input.
 
